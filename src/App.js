3ddcb0ab8b7fb4915d236866770b7c2f9811f882
Caleb Addison <caleb.h.addison@gmail.com>
2024-02-04 13:43:42 +1100

    Implemented bootstrap and improved some workflows

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import ApiCaller from './components/ApiCaller';
 import DataExporter from './components/DataExporter';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 const App = () => {
     const [data, setData] = useState([]);
@@ -12,6 +15,11 @@ const App = () => {
 
     return (
         <div>
+            <Navbar variant="light" className="navbar-custom">
+                <Container>
+                    <Navbar.Brand>Wunderground API to .xlsx</Navbar.Brand>
+                </Container>
+            </Navbar>
             <ApiCaller onDataReceived={handleDataReceived} />
             <DataExporter data={data} />
         </div>
--- src/components/ApiCaller.js
+++ src/components/ApiCaller.js
@@ -3,26 +3,41 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const ApiCaller = ({ onDataReceived }) => {
     const [endpoint, setEndpoint] = useState('');
+    const [endpointText, setEndpointText] = useState('');
     const [pathParams, setPathParams] = useState({
         apiKey: '',
         stationId: '',
         unit: '',
     });
-    const [apiParams, setApiParams] = useState({
-        numericPrecision: 'decimal',
-        foo: true,
-        bar: 12,
-    });
     const [rememberMe, setRememberMe] = useState(false);
-    const [jsonData, setJsonData] = useState('No output to display');
+    const [isLoading, setIsLoading] = useState(false);
     const [outputMessage, setOutputMessage] = useState({
         message: '',
         class: '',
     });
+    const [queryTimestamp, setQueryTimestamp] = useState(null);
+    const [apiParams, setApiParams] = useState({
+        numericPrecision: 'decimal',
+    });
+    const [jsonData, setJsonData] = useState('');
 
+    // References
+    const endpointRef = useRef(null);
+    const apikeyRef = useRef(null);
+    const stationIdRef = useRef(null);
+    const unitRef = useRef(null);
+
+    // Values
     const baseApiUrl = 'https://api.weather.com/v2/pws/';
     const apiEndpoints = [
         {
@@ -30,7 +45,11 @@ const ApiCaller = ({ onDataReceived }) => {
             url: 'observations/current',
             params: ['numericPrecision'],
         },
-        { text: 'todo', url: 'foo/bar', params: ['foo'] },
+        {
+            text: '7 Day History',
+            url: 'dailysummary/7day',
+            params: ['numericPrecision'],
+        },
     ];
     const units = [
         { value: 'e', displayText: 'English units' },
@@ -38,11 +57,13 @@ const ApiCaller = ({ onDataReceived }) => {
         { value: 'h', displayText: 'Hybrid units (UK)' },
         { value: 's', displayText: 'Metric SI units' },
     ];
-    const [isLoading, setIsLoading] = useState(false);
 
-    const endpointRef = useRef();
+    const setEndpointHelper = (url, text) => {
+        setEndpoint(url);
+        setEndpointText(text);
+    };
 
-    // Load the rememberMe value from the cookie when the component mounts
+    // Hooks
     useEffect(() => {
         const rememberMeCookie = Cookies.get('rememberMe');
         setRememberMe(rememberMeCookie === 'true');
@@ -57,6 +78,12 @@ const ApiCaller = ({ onDataReceived }) => {
             unit: unitCookie || '',
         });
     }, []);
+
+    // Helpers
+    function isParamInEndpoint(paramName) {
+        const endpointObj = apiEndpoints.find((obj) => obj.url === endpoint);
+        return endpointObj && endpointObj.params.includes(paramName);
+    }
 
     const constructApiUrl = () => {
         return (
@@ -82,7 +109,7 @@ const ApiCaller = ({ onDataReceived }) => {
         Cookies.remove('unit');
     };
 
-    function getFilteredApiParams() {
+    const getFilteredApiParams = () => {
         const endpointParams =
             apiEndpoints.find((item) => item.url === endpoint)?.params || [];
         const filteredParams = {};
@@ -99,17 +126,52 @@ const ApiCaller = ({ onDataReceived }) => {
 
         console.log('filter: ', apiParams, filteredParams);
         return filteredParams;
-    }
+    };
+
+    const setRememberMeHelper = (checked) => {
+        setRememberMe(checked);
+        if (!checked) {
+            clearCookies();
+        }
+    };
 
     const fetchData = async () => {
-        // DEBUG
-        console.log('apiParams:', apiParams);
+        // Reset message
+        setOutputMessage({
+            message: '',
+            class: '',
+        });
+        setQueryTimestamp(null);
 
+        // Validate inputs
         if (!endpoint) {
             endpointRef.current.focus();
             setOutputMessage({
                 message: 'Please specify endpoint',
-                class: 'warning',
+                class: 'info',
+            });
+            return;
+        }
+
+        if (!pathParams.apiKey) {
+            apikeyRef.current.focus();
+            setOutputMessage({
+                message: 'Please specify API Key',
+                class: 'info',
+            });
+            return;
+        } else if (!pathParams.stationId) {
+            stationIdRef.current.focus();
+            setOutputMessage({
+                message: 'Please specify Station ID',
+                class: 'info',
+            });
+            return;
+        } else if (!pathParams.unit) {
+            unitRef.current.focus();
+            setOutputMessage({
+                message: 'Please specify Unit',
+                class: 'info',
             });
             return;
         }
@@ -121,7 +183,7 @@ const ApiCaller = ({ onDataReceived }) => {
                 params: getFilteredApiParams(),
             });
             onDataReceived(response.data);
-            console.log(response.data);
+            setQueryTimestamp(new Date().toISOString());
             setJsonData(JSON.stringify(response.data, null, 2));
             setOutputMessage({
                 message: 'Data fetched successfully',
@@ -132,154 +194,232 @@ const ApiCaller = ({ onDataReceived }) => {
             console.error(error);
             let errorMessage = 'An error occured while fetching data';
             if (error.message && error.code) {
-                errorMessage = `${error.code} - ${error.message}`;
+                errorMessage = `${error.code} - ${error.message}. Please check the parameter values are correct.`;
             }
             setOutputMessage({
                 message: errorMessage,
-                class: 'error',
+                class: 'danger',
             });
-            setJsonData('No output to display');
+            setJsonData('');
         } finally {
             setIsLoading(false);
         }
     };
 
     return (
-        <div>
-            <b>API Endpoint</b>
+        <Form className="m-3 api-form mx-auto">
+            <Form.Group as={Row} className="mb-3" controlId="formEndpoint">
+                <Form.Label column sm="2">
+                    Endpoint
+                </Form.Label>
+                <Col sm="10">
+                    <InputGroup className="mb-3 dropdown-button-custom">
+                        <DropdownButton
+                            variant={endpoint ? 'light' : 'primary'}
+                            title={
+                                endpointText ? endpointText : 'Select endpoint'
+                            }
+                        >
+                            {apiEndpoints.map((endpoint, i) => (
+                                <Dropdown.Item
+                                    key={i}
+                                    onClick={() => {
+                                        setEndpointHelper(
+                                            endpoint.url,
+                                            endpoint.text,
+                                        );
+                                    }}
+                                >
+                                    {endpoint.text}
+                                </Dropdown.Item>
+                            ))}
+                        </DropdownButton>
+                        <Form.Control
+                            aria-label="Text input with dropdown button"
+                            ref={endpointRef}
+                            readOnly
+                            value={`${baseApiUrl}${endpoint}`}
+                        />
+                    </InputGroup>
+                </Col>
+            </Form.Group>
 
-            <div>
-                <label className="input-label">Endpoint</label>
-                <select
-                    className="input-field"
-                    ref={endpointRef}
-                    value={endpoint}
-                    onChange={(e) => setEndpoint(e.target.value)}
-                >
-                    <option hidden disabled value="">
-                        Select...
-                    </option>
-                    {apiEndpoints.map((endpoint, i) => (
-                        <option key={i} value={endpoint.url}>
-                            {endpoint.text}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            <hr />
-            <b>Path Parameters</b>
-            <div>
-                <label className="input-label">API Key</label>
-                <input
-                    className="input-field"
-                    type="text"
-                    value={pathParams.apiKey}
-                    onChange={(e) =>
-                        setPathParams({ ...pathParams, apiKey: e.target.value })
-                    }
-                    placeholder="API Key"
-                />
-            </div>
-            <div>
-                <label className="input-label">Station ID</label>
-                <input
-                    className="input-field"
-                    type="text"
-                    value={pathParams.stationId}
-                    onChange={(e) =>
-                        setPathParams({
-                            ...pathParams,
-                            stationId: e.target.value,
-                        })
-                    }
-                    placeholder="Station ID"
-                />
-            </div>
-            <div>
-                <label className="input-label">Unit</label>
-                <select
-                    className="input-field"
-                    value={pathParams.unit}
-                    onChange={(e) =>
-                        setPathParams({ ...pathParams, unit: e.target.value })
-                    }
-                >
-                    <option hidden disabled value="">
-                        Select...
-                    </option>
-                    {units.map((unit, i) => (
-                        <option key={i} value={unit.value}>
-                            {unit.displayText}
-                        </option>
-                    ))}
-                </select>
-            </div>
-
-            <div>
-                <label>
-                    <input
-                        type="checkbox"
-                        checked={rememberMe}
-                        onChange={(e) => setRememberMe(e.target.checked)}
+            <Form.Group as={Row} className="mb-3" controlId="formApiKey">
+                <Form.Label column sm="2">
+                    API Key
+                </Form.Label>
+                <Col sm="10">
+                    <Form.Control
+                        placeholder="..."
+                        value={pathParams.apiKey}
+                        ref={apikeyRef}
+                        onChange={(e) =>
+                            setPathParams({
+                                ...pathParams,
+                                apiKey: e.target.value,
+                            })
+                        }
                     />
-                    Remember Path Parameters
-                </label>
-            </div>
+                </Col>
+            </Form.Group>
 
-            <hr />
-            <div>
-                <b>Query Parameters</b>
-            </div>
-
-            {endpoint === 'observations/current' && (
-                <div>
-                    <label className="input-label">Numeric Precision</label>
-                    <select
-                        className="input-field"
-                        value={apiParams.numericPrecision}
-                        onChange={(e) => {
-                            setApiParams({
-                                ...apiParams,
-                                numericPrecision: e.target.value,
-                            });
-                        }}
+            <Form.Group as={Row} className="mb-3" controlId="formStationId">
+                <Form.Label column sm="2">
+                    Station ID
+                </Form.Label>
+                <Col sm="5">
+                    <Form.Control
+                        placeholder="..."
+                        ref={stationIdRef}
+                        value={pathParams.stationId}
+                        onChange={(e) =>
+                            setPathParams({
+                                ...pathParams,
+                                stationId: e.target.value,
+                            })
+                        }
+                    />
+                </Col>
+                <Form.Label column sm="1">
+                    Unit
+                </Form.Label>
+                <Col sm="4">
+                    <Form.Select
+                        value={pathParams.unit}
+                        ref={unitRef}
+                        onChange={(e) =>
+                            setPathParams({
+                                ...pathParams,
+                                unit: e.target.value,
+                            })
+                        }
                     >
-                        <option value="decimal">Decimal</option>
-                        <option value="">Integer</option>
-                    </select>{' '}
-                </div>
+                        <option hidden disabled value="">
+                            Select...
+                        </option>
+                        {units.map((unit, i) => (
+                            <option key={i} value={unit.value}>
+                                {unit.displayText}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Col>
+            </Form.Group>
+
+            <Form.Group
+                as={Row}
+                className="mb-3"
+                controlId="formRememberParams"
+            >
+                <Col sm={12}>
+                    <Form.Check
+                        type="checkbox"
+                        label="Remember base parameters"
+                        checked={rememberMe}
+                        onChange={(e) => setRememberMeHelper(e.target.checked)}
+                        className="right-align-checkbox"
+                    />
+                </Col>
+            </Form.Group>
+
+            {isParamInEndpoint('numericPrecision') && (
+                <Form.Group as={Row} className="mb-3">
+                    <Form.Label column sm="2" className="input-label">
+                        Numeric Precision
+                    </Form.Label>
+                    <Col sm="5">
+                        <Form.Select
+                            className="input-field"
+                            value={apiParams.numericPrecision}
+                            onChange={(e) => {
+                                setApiParams({
+                                    ...apiParams,
+                                    numericPrecision: e.target.value,
+                                });
+                            }}
+                        >
+                            <option value="decimal">Decimal</option>
+                            <option value="">Integer</option>
+                        </Form.Select>
+                    </Col>
+                </Form.Group>
             )}
 
-            <button
-                className="fetch-button"
-                onClick={fetchData}
-                disabled={isLoading}
-            >
-                {isLoading ? (
-                    <>
-                        <FontAwesomeIcon icon={faSpinner} spin /> Loading
-                    </>
-                ) : (
-                    'Fetch Data'
-                )}
-            </button>
+            <Form.Group className="d-flex justify-content-center">
+                <Button
+                    variant="success"
+                    onClick={fetchData}
+                    disabled={isLoading}
+                    style={{ width: '200px' }}
+                >
+                    {isLoading ? (
+                        <>
+                            <FontAwesomeIcon icon={faSpinner} spin /> Loading
+                        </>
+                    ) : (
+                        'Fetch Data'
+                    )}
+                </Button>
+            </Form.Group>
+
+            <Form.Group className="mt-2 text-center">
+                <Form.Label className={outputMessage.class}>
+                    {outputMessage.message}
+                </Form.Label>
+            </Form.Group>
 
             <hr />
 
-            <b>Results</b>
-            <div>
-                <i className={outputMessage.class}>{outputMessage.message}</i>
+            <Form.Group>
+                <Row>
+                    <Col>
+                        <Form.Label>Results</Form.Label>
+                    </Col>
+                </Row>
+                <Row style={{ marginTop: '-15px' }}>
+                    <Col>
+                        <Form.Label
+                            className="text-muted"
+                            style={{ fontSize: '0.8rem', fontStyle: 'italic' }}
+                        >
+                            {queryTimestamp &&
+                                `(query completed ${queryTimestamp})`}
+                        </Form.Label>
+                    </Col>
+                </Row>
+                <Row>
+                    <Form.Control
+                        as="textarea"
+                        className="monospace"
+                        readOnly
+                        value={jsonData ? jsonData : 'No output to display'}
+                        rows={jsonData ? 20 : 5}
+                    />
+                </Row>
+            </Form.Group>
+
+            <div style={{ display: 'none' }}>
+                <hr />
+                <b>debugging</b>
+                <br />
+                <label>endpoint: {endpoint}</label>
+                <br />
+                <label>endpointText: {endpointText}</label>
+                <br />
+                {Object.entries(pathParams).map(([key, value]) => (
+                    <div key={key}>
+                        <label>
+                            {key}: {value}
+                        </label>
+                        <br />
+                    </div>
+                ))}
+                <label>outputMessage: {JSON.stringify(outputMessage)}</label>
+                <br />
+                <label>apiParams: {JSON.stringify(apiParams)}</label>
+                <br />
             </div>
-            <div>
-                <textarea
-                    value={jsonData}
-                    className="json-output"
-                    rows={40}
-                    readOnly
-                />
-            </div>
-        </div>
+        </Form>
     );
 };
 
